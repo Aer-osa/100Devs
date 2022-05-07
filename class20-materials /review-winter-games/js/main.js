72bc1f0344cef3f56dfb866c61ea41b9605a4a2d
Aer-osa <eric.n.long@gmail.com>
2022-05-07 11:28:59 -0700

    Finish exercise

--- class20-materials /review-winter-games/js/main.js
+++ class20-materials /review-winter-games/js/main.js
@@ -1,6 +1,14 @@
 //Create a function that takes in an array of numbers. Return a new array containing every even number from the original array (do not use map or filter)
 let array = [1,2,3,4]
-function onlyEven(array) {
-    for (let i = 0; i < array.length; i++){
+function onlyEven(nums) {
+    let evens = []
 
-    }
+    nums.forEach( n => {
+if(n % 2 === 0){
+    evens.push(n)
+}
+
+    })
+    return evens
+}
+console.log(onlyEven([1,2,3,4,5,6,7,8]))
