4e8f8c6904c21e8d0270be76c90a28e63246eb9a
Aer-osa <eric.n.long@gmail.com>
2022-05-06 19:17:06 -0700

    Complete exercise

--- class20-materials /review-winter-games/js/main.js
+++ class20-materials /review-winter-games/js/main.js
@@ -1,1 +1,6 @@
 //Create a function that takes in an array of numbers. Return a new array containing every even number from the original array (do not use map or filter)
+let array = [1,2,3,4]
+function onlyEven(array) {
+    for (let i = 0; i < array.length; i++){
+
+    }
