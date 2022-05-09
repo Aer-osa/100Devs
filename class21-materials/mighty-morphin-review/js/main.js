2ef2b806d73c186b19198e61c18c679667b19497
Aer-osa <eric.n.long@gmail.com>
2022-05-09 12:17:53 -0700

    Add exercise solution

--- class21-materials/mighty-morphin-review/js/main.js
+++ class21-materials/mighty-morphin-review/js/main.js
@@ -1,11 +1,21 @@
 // *Variables*
 // Declare a variable, reassign it to your fav holiday, make sure it is in all caps, and print the value to the console
-
+let favoriteHoliday = 'Easter'
+favoriteHoliday =favoriteHoliday.toUpperCase
+console.log(favoriteHoliday)
 //Declare a variable, assign it a string, alert the last three characters in the string (Use your google-fu and the MDN)
+let lastThree = 'Cheese'
+
+    alert(lastThree.slice(-3))
+
 
 // *Functions*
 // Create a function that takes in 5 numbers. Subtract all five from 100. Alert the absolute value of the difference. Call the function.
-
+function takeFiveNumber(n1, n2, n3, n4, n5){
+    let difference = 100 - n1 - n2 - n3 - n4 - n5
+    alert(Math.abs(difference))
+}
+takeFiveNumber(27,48,75,21,35)
 
 // Create a function that takes in 3 numbers. Console log lowest and highest values. Call the function.
 
