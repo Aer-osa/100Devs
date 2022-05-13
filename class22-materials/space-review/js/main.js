71b455cb1431fbcad8bc73e4e33e6d33b24e7d25
Aer-osa <eric.n.long@gmail.com>
2022-05-13 16:36:00 -0700

    Complete review

--- class22-materials/space-review/js/main.js
+++ class22-materials/space-review/js/main.js
@@ -2,6 +2,10 @@
 
 //Create and array of numbers. Sum all of the numbers. Alert the sum.
 
+    let arr = [13,42,15,3]
+    let sum = arr.reduce((partialSum, a) => partialSum + a, 0)
+    console.log(sum)
+
 //Create a function that takes in an array of numbers
 //Return a new array of numbers that is every original number squared
 
