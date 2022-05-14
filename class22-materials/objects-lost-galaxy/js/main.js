63282a82e0d9df0a8e044644921a97d89a6bb811
Aer-osa <eric.n.long@gmail.com>
2022-05-13 21:43:43 -0700

    Finish exercise

--- class22-materials/objects-lost-galaxy/js/main.js
+++ class22-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,19 @@
 //Create a mouse object that has four properties and three methods
+let mouse = {}
+
+mouse.brand = 'Perixx'
+mouse.color = 'Black'
+mouse.model = 'Mx Ergo'
+mouse.wireless = true
+
+mouse.leftClick = function(){
+    console.log('Left Cliccck')
+
+}
+mouse.rightClick = function() {
+    console.log('Right clickkkk')
+
+}
+mouse.scroll = function(){
+    console.log('Jump jump jump')
+}
