a594de3c2cae075ea3dda7672c0fc1c90bc5f5c7
Aer-osa <eric.n.long@gmail.com>
2022-05-19 16:46:23 -0700

    Commit new lesson

--- class27-materials/objects-netflix-tv-shows/js/main.js
+++ class27-materials/objects-netflix-tv-shows/js/main.js
@@ -1,3 +1,21 @@
 //Create a Netflix TV Show class with a constructor that makes Netflix TV Shows with 4 properties and 3 methods
+class MakeShow{
+    constructor(showShow,castShow,locationShow,dateShow){
+        this.show = showShow
+        this.cast = castShow
+        this.location = locationShow
+        this.date = dateShow
+    }
+    play(){
+        alert('Play Show')
+    }
+    skip(){
+        alert('skip this shit')
+    }
+    delete(){
+        alert('this trash is garbage')
+    }
+    
+}
 
-
+let peakyBlinders = new MakeShow('Peaky Blinders','Unknown','england','2020')
