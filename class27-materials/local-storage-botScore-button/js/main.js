2073cc5c9c4ba491982f615aa8a42e4115d2563a
Aer-osa <eric.n.long@gmail.com>
2022-05-21 17:59:44 -0700

    Chchchchanges

--- class27-materials/local-storage-botScore-button/js/main.js
+++ class27-materials/local-storage-botScore-button/js/main.js
@@ -1,3 +1,11 @@
 //Create a button that adds 1 to a botScore stored in localStorage 
+localStorage.setItem('botScore', 0 )
 
+document.querySelector('button').addEventListener('click', addAnothaOne)
+
+function addAnothaOne(){
+    let botScoreVal; = localStorage.getItem('botScore')
+    botScoreVal += 1
+    localStorage.setItem('botScore',, botScoreVal)
+}
 
