ab8f3314e4eb5d8141e4c5d2b2471389cb13b4e5
Aer-osa <eric.n.long@gmail.com>
2022-05-24 19:09:02 -0700

    Add li to the DOM

--- class28-materials/dnd/js/main.js
+++ class28-materials/dnd/js/main.js
@@ -8,7 +8,19 @@ function getFetch(){
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
-       
+        console.log(data.subclasses)
+        // console.log(data.subclasses[0].name)
+        // console.log(data.subclasses[1].name) 
+        data.subclasses.forEach(obj => {
+            console.log(obj.name)
+            //Create an li
+            const li = document.createElement('li')
+            //add text to li
+            li.textContent = obj.name
+            //append the li to the ul
+            document.querySelector('ul').appendChild(li)
+        })
+        
       })
       .catch(err => {
           console.log(`error ${err}`)
