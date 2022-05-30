c9d75958326e802e6decc01c7d4de2f9f660b972
Aer-osa <eric.n.long@gmail.com>
2022-05-29 20:52:17 -0700

    Comment out vals 3-8

--- homework/blackjack-game/js/main.js
+++ homework/blackjack-game/js/main.js
@@ -24,20 +24,20 @@ function getFetch(){
         console.log(data)
         let val1 = Number(cardValue( data.cards[0].value ))
         let val2 = Number(cardValue( data.cards[1].value ))
-        let val3 = Number(cardValue( data.cards[2].value ))
-        let val4 = Number(cardValue( data.cards[3].value ))
-        let val5 = Number(cardValue( data.cards[4].value ))
-        let val6 = Number(cardValue( data.cards[5].value ))
-        let val7 = Number(cardValue( data.cards[6].value ))
-        let val8 = Number(cardValue( data.cards[7].value ))
+        // let val3 = Number(cardValue( data.cards[2].value ))
+        // let val4 = Number(cardValue( data.cards[3].value ))
+        // let val5 = Number(cardValue( data.cards[4].value ))
+        // let val6 = Number(cardValue( data.cards[5].value ))
+        // let val7 = Number(cardValue( data.cards[6].value ))
+        // let val8 = Number(cardValue( data.cards[7].value ))
         document.querySelector('#player1c1').src = data.cards[0].image
         document.querySelector('#player1c2').src = data.cards[1].image
-        document.querySelector('#player2c1').src = data.cards[2].image
-        document.querySelector('#player2c2').src = data.cards[3].image
-        document.querySelector('#player1c3').src = data.cards[4].image
-        document.querySelector('#player1c4').src = data.cards[5].image
-        document.querySelector('#player2c3').src = data.cards[6].image
-        document.querySelector('#player2c4').src = data.cards[7].image
+        // document.querySelector('#player2c1').src = data.cards[2].image
+        // document.querySelector('#player2c2').src = data.cards[3].image
+        // document.querySelector('#player1c3').src = data.cards[4].image
+        // document.querySelector('#player1c4').src = data.cards[5].image
+        // document.querySelector('#player2c3').src = data.cards[6].image
+        // document.querySelector('#player2c4').src = data.cards[7].image
         // if(val1 > val2){
         //   document.querySelector('h3').innerText = 'Player 1 WON!'
         // }else if(val1 < val2){
