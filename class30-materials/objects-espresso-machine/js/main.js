fde0c01e76140b6134d24966d64b01545eeb28c4
Aer-osa <eric.n.long@gmail.com>
2022-05-30 15:17:29 -0700

    Move stuff around

--- class30-materials/objects-espresso-machine/js/main.js
+++ class30-materials/objects-espresso-machine/js/main.js
@@ -1,1 +1,19 @@
 //Create an espresso machine constructor that makes machines with 4 properties and 3 methods
+class MakeEspressoMachine {
+    constructor(machineMake,machineModel,machineFeature,machinePressure){
+        this.make = machineMake
+        this.model = machineModel
+        this.feature = machineFeature
+        this.pressure = machinePressure
+
+    }
+    grind(){
+        alert("Espresso grind")
+    }
+    autoTamp(){
+        alert("Tamp tamp tamp")
+    }
+    foam(){
+        alert("That nice microfoam")
+    }
+}
