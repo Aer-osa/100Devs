8b4607c6e4f5a2c63a3882ae024564d5b0f0adc1
Aer-osa <eric.n.long@gmail.com>
2022-06-05 17:32:48 -0700

    Add hit function

--- homework/blackjack-game/js/main.js
+++ homework/blackjack-game/js/main.js
@@ -24,18 +24,22 @@ function newDraw(){
         console.log(data)
         let val1 = Number(cardValue( data.cards[0].value ))
         let val2 = Number(cardValue( data.cards[1].value ))
+        let val3 = Number(cardValue( data.cards[2].value ))
+        let val4 = Number(cardValue( data.cards[3].value ))
+
         
-        
-        // let val5 = Number(cardValue( data.cards[4].value ))
-        // let val6 = Number(cardValue( data.cards[5].value ))
+        //
+        // 
         // let val7 = Number(cardValue( data.cards[6].value ))
         // let val8 = Number(cardValue( data.cards[7].value ))
         document.querySelector('#player1c1').src = data.cards[0].image
         document.querySelector('#player1c2').src = data.cards[1].image
         
+        document.querySelector('#player2c1').src = data.cards[2].image
+        document.querySelector('#player2c2').src = data.cards[3].image
         
-        // document.querySelector('#player1c3').src = data.cards[4].image
-        // document.querySelector('#player1c4').src = data.cards[5].image
+        // 
+        // 
         // document.querySelector('#player2c3').src = data.cards[6].image
         // document.querySelector('#player2c4').src = data.cards[7].image
     
@@ -49,18 +53,17 @@ function newDraw(){
 document.querySelector('#Hit').addEventListener('click', nextCard)
 
 function nextCard(){
-  const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=4`
+  const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
+        // let val5 = Number(cardValue( data.cards[4].value ))
+        // let val6 = Number(cardValue( data.cards[5].value ))
+        document.querySelector('#player1c3').src = data.cards[0].image
         
-        let val3 = Number(cardValue( data.cards[2].value ))
-        let val4 = Number(cardValue( data.cards[3].value ))
-
-        document.querySelector('#player1c3').src = data.cards[2].image
-        document.querySelector('#player1c4').src = data.cards[3].image
+        document.querySelector('#player2c3').src = data.cards[1].image
       })
       .catch(err => {
           console.log(`error ${err}`)
