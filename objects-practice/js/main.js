74ef2461f6782bd12262f5499743532d874acd46
Aer-osa <eric.n.long@gmail.com>
2022-06-05 21:10:19 -0700

    Complete exercise

--- objects-practice/js/main.js
+++ objects-practice/js/main.js
@@ -66,7 +66,7 @@ class Contractor{
         console.log(`Hello, I am on the ${this._role} team at #100Devs`)
     }
 }
-let machi = new Contractor('The Machine','Front-end')
+
 
 class Front extends Contractor{
     constructor(name,role,tech){
@@ -80,6 +80,27 @@ class Front extends Contractor{
         console.log(`Hello, I am on the ${this._role} team at #100Devs and I use ${this._tech}`)
     }
 }
+class Back extends Contractor{
+        constructor(name,role,tech){
+            super(name,role)
+            this._tech = tech
+        }
+        get tech(){
+            return this._tech
+        }
+        sayHello(){
+            console.log(`Hello, I am on the ${this._role} team at #100Devs and I use ${this._tech}`)
+        }
+    }
+    let bob = new Contractor('Bob','Front-end')
+    let simba = new Front('Simba','Front-end','React')
+    let machi = new Back('The Machine','Back-end','Node')
+
+    let agency = [bob,simba,machi]
+
+    for(person of agency){
+        person.sayHello()
+    }
 
 
 
