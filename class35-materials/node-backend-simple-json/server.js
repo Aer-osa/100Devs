2a6f2b52df6969aed53a08ae92310a5536ee4b85
Aer-osa <eric.n.long@gmail.com>
2022-06-10 19:03:31 -0700

    Add coin flip game

--- class35-materials/node-backend-simple-json/server.js
+++ class35-materials/node-backend-simple-json/server.js
@@ -14,59 +14,52 @@ const server = http.createServer((req, res) => {
     });
   }
 
-
   const page = url.parse(req.url).pathname;
   const params = querystring.parse(url.parse(req.url).query);
   console.log(page);
-  if (page == '/') {
-    readWrite('index.html','text/html')
-  }
-  else if (page == '/otherpage') {
-    readWrite('otherpage.html','text/html')
-  }
-  else if (page == '/otherotherpage') {
-    readWrite('otherotherpage.html','text/html')
-  }
-  else if (page == '/api') {
-    if('student' in params){
-      if(params['student']== 'leon'){
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        const objToJson = {
-          name: "leon",
-          status: "Boss Man",
-          currentOccupation: "Baller"
-        }
-        res.end(JSON.stringify(objToJson));
-      }//student = leon
-      else if(params['student'] != 'leon'){
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        const objToJson = {
-          name: "asshole",
-          status: "unknown",
-          currentOccupation: "unknown"
-        }
-        res.end(JSON.stringify(objToJson));
-      }//student != leon
-    }//student if
-  }//else if
-  else if (page == '/css/style.css'){
-    
-    fs.readFile('css/style.css', function(err, data) {
-      res.write(data);
-      res.end();
-    });
-  }else if (page == '/js/main.js'){
-    readWrite('js/main.js','text/javascript')
-  }else{
-    figlet('404!!', function(err, data) {
-      if (err) {
-          console.log('Something went wrong...');
-          console.dir(err);
-          return;
+
+  switch (page) {
+    case '/':
+      readWrite('index.html','text/html')
+      break;
+    case '/otherpage':
+      readWrite('otherpage.html','text/html')
+      break;
+    case '/otherotherpage':
+      readWrite('otherotherpage.html','text/html')
+      break;
+    case '/api':
+      let flipResult =  "type flip in the box"
+      if (params['student'] == 'flip') {
+        flipResult = Math.random() <= .5 ? 'heads' :'tails'
       }
-      res.write(data);
-      res.end();
-    });
+
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        const objToJson = {
+          name: flipResult
+        }
+        res.end(JSON.stringify(objToJson));
+      break;
+    case '/css/style.css':
+      fs.readFile('css/style.css', function(err, data) {
+        res.write(data);
+        res.end();
+      });
+      break;
+    case '/js/main.js':
+      readWrite('js/main.js','text/javascript')
+      break;
+    default:
+      figlet('404!!', function(err, data) {
+        if (err) {
+            console.log('Something went wrong...');
+            console.dir(err);
+            return;
+        }
+        res.write(data);
+        res.end();
+      });
+      break;
   }
 });
 
