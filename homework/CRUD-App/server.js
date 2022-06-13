48da92d3f13941776ac927cf91228d1c55cadb32
Aer-osa <eric.n.long@gmail.com>
2022-06-12 18:25:07 -0700

    Install nodemon

--- homework/CRUD-App/server.js
+++ homework/CRUD-App/server.js
@@ -1,10 +1,16 @@
 
 const express = require('express')
+const bodyParser= require('body-parser')
 const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(3000, function(){
     console.log('listening on 3000')
 })
-app.get('/', function(req, res){
-    res.send('hello world')
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
 })
+app.post('/quotes', (req, res) => {
+    console.log('Hellooooooooooooooooo!')
+  })
