e198edfd6f6bcb318df279d974d2a89e2284db6e
Aer-osa <eric.n.long@gmail.com>
2022-06-21 16:51:19 -0700

    Changes

--- homework/the-orville-cast-api/server.js
+++ homework/the-orville-cast-api/server.js
@@ -37,6 +37,6 @@ app.get('/api/:crewName', (request, response)=>{
     }
 })
 
-app.listen(PORT, ()=>{
+app.listen(process.env.PORT || PORT, ()=>{
     console.log(`The server is running on ${PORT}... better go catch it!`)
 })
