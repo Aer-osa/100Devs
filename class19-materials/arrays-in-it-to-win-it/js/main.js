b80cdf807e9d295b01e807155c6dded667b26c39
Aer-osa <eric.n.long@gmail.com>
2022-06-25 14:38:47 -0700

    Delete Class materials for space

--- class19-materials/arrays-in-it-to-win-it/js/main.js
+++
@@ -1,8 +0,0 @@
-//Create an array of movies with at least three movies.
-let arrMovies = ['Bambi','Star Wars','Scooby Doo']
-//Using the array from above, store the first movie in a variable
-let butts = arrMovies[0]
-//Get the length of the original array and store it in a new variable
-let cheeks = arrMovies.length
-//Get the last element in that array and store it in a new variable. What if your array was really large and you didn't know the last index? Would your solution still work?
-let thighs = arrMovies [arrMovies.length - 1]
