234f5037624457c644d921fb1f30069654b2a147
Aer-osa <eric.n.long@gmail.com>
2022-07-11 20:50:42 -0700

    Update POST

--- node-project/index.js
+++ node-project/index.js
@@ -31,9 +31,31 @@ let notes = [
     response.json(notes)
   })
 
+  const generateId = () => {
+    const maxId = notes.length > 0
+      ? Math.max(...notes.map(n => n.id))
+      : 0
+    return maxId + 1
+  }
+  
   app.post('/api/notes', (request, response) => {
-    const note = request.body
-    console.log(note)
+    const body = request.body
+  
+    if (!body.content) {
+      return response.status(400).json({ 
+        error: 'content missing' 
+      })
+    }
+  
+    const note = {
+      content: body.content,
+      important: body.important || false,
+      date: new Date(),
+      id: generateId(),
+    }
+  
+    notes = notes.concat(note)
+  
     response.json(note)
   })
 
