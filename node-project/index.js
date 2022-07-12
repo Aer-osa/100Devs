8086c892110216ac4867e7afe8875fc7aad0338c
Aer-osa <eric.n.long@gmail.com>
2022-07-11 20:13:13 -0700

    Add DELETE function

--- node-project/index.js
+++ node-project/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let notes = [
     {
       id: 1,
@@ -29,15 +31,21 @@ let notes = [
     response.json(notes)
   })
 
-//   app.get('/api/notes/:id', (request, response) => {
-//     const id = Number(request.params.id)
-//     const note = notes.find(note => note.id === id)
-//     if (note) {
-//         response.json(note)
-//       } else {
-//         response.status(404).end()
-//       }
-//   })
+  app.post('/api/notes', (request, response) => {
+    const note = request.body
+    console.log(note)
+    response.json(note)
+  })
+
+  app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const note = notes.find(note => note.id === id)
+    if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+  })
 
   app.delete('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
