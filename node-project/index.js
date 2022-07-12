aa8155a36bbdcd7b8b0e07b7d291af9bb37a9452
Aer-osa <eric.n.long@gmail.com>
2022-07-11 19:55:54 -0700

    Add Express

--- node-project/index.js
+++ node-project/index.js
@@ -1,6 +1,5 @@
-console.log("Hello world!")
-
-const http = require('http')
+const express = require('express')
+const app = express()
 
 let notes = [
     {
@@ -22,11 +21,31 @@ let notes = [
       important: true
     }
   ]
+  app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+  })
+  
+  app.get('/api/notes', (request, response) => {
+    response.json(notes)
+  })
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'application/json' })
-  response.end(JSON.stringify(notes))
-})
+//   app.get('/api/notes/:id', (request, response) => {
+//     const id = Number(request.params.id)
+//     const note = notes.find(note => note.id === id)
+//     if (note) {
+//         response.json(note)
+//       } else {
+//         response.status(404).end()
+//       }
+//   })
+
+  app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+  
+    response.status(204).end()
+  })
+
 
 const PORT = 3001
 app.listen(PORT)
